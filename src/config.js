76bfde67a0edb5a4110a729f957955f9ae0eec2f
Miqop120 <112582093+Miqop120@users.noreply.github.com>
2022-09-08 19:10:17 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 module.exports = {
-    token: "",
-    prefix: "",
-    ownerID: "",
+    token: "MTAxNzQ0NzQ5NjY4OTEzNTY2Nw.G4RGX1.QLJCAtKi5INPBP0sBlSNd_8Ro4cfZL8_0IoGSw",
+    prefix: "$",
+    ownerID: "871354873436839936",
     SpotifyID: "",
     SpotifySecret: "",
-    mongourl: "",
+    mongourl: "mongodb+srv://Miq:hastyar12212@cluster0.euwrbdi.mongodb.net/?retryWrites=true&w=majority",
     embedColor: "",
-    logs: "",
+    logs: "1000096509662072929",
 
   nodes: {
      
